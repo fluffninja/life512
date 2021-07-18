a9565b203e5e383eb57201bb34318763403d1669
fluffninja <fluffninja@outlook.com>
2021-07-18 01:29:38 +0100

    Add game of life

--- boot.asm
+++ boot.asm
@@ -4,59 +4,172 @@
 
 
 Main:
-    jmp     long 0:Main.Norm
+    jmp     0:Main.Norm
 .Norm:
     xor     ax, ax
-    mov     ds, ax
-    mov     es, ax
     mov     ss, ax
     mov     sp, 0x7c00
 
     ; Set Video Mode
-    mov     ax, 0x05
+    mov     al, 0x13
     int     0x10
 
-    call    TickAndRender
 
-    cli
-    hlt
+    ; Initialise board.
+    mov     ax, 0x1000
+    mov     es, ax
+
+    xor     di, di
+    mov     cx, 256 * 128
+.Body:
+    mov     ax, cx
+    shr     ax, 1
+    and     ax, 1
+    stosb
+    loop    .Body
+
+
+MainLoop:
+    mov     cx, 0x1000
+    mov     dx, 0x2000
+.Body:
+    mov     ds, cx
+    mov     es, dx
+    push    cx
+    push    dx
+    call    TickAndRender
+    pop     dx
+    pop     cx
+    xchg    cx, dx
+    jmp     .Body
 
 
 TickAndRender:
-    xor     bh, bh
-    mov     ah, 0xc
-
+    ; cl = X
+    ; ch = Y
     xor     cx, cx
+    xor     bp, bp
     jmp     .YCond
 
 .Body:
-
-    mov     bl, cl
-    xor     bl, ch
-    mov     al, 0
-    and     bl, 1
-    jz      short .Skip
-    dec     al
-.Skip:
-
     push    cx
-    movzx   dx, ch
-    movzx   cx, cl
-    int     0x10
-    pop     cx
 
+    ; al = neighbour count
+    xor     ax, ax
+
+    ; Current row:
+    ; bl = X
+    ; bh = Y
+    mov     bx, cx
+
+    dec     bl
+    add     al, [bx]
+    inc     bl
+    mov     dl, [bx]   ; dl = Current
+    inc     bl
+    add     al, [bx]
+
+    ; Previous row:
+    ; bh = Y - 1
+    ; bh already Y
+    dec     bh
+    and     bh, 0x7f
+
+    add     al, [bx]
+    dec     bl
+    add     al, [bx]
+    dec     bl
+    add     al, [bx]
+
+    ; Next row:
+    ; bh = Y + 1
+    inc     bh
+    inc     bh
+    and     bh, 0x7f
+
+    add     al, [bx]
+    inc     bl
+    add     al, [bx]
+    inc     bl
+    add     al, [bx]
+
+    test    dl, dl
+    jz      .CurrentlyDead
+
+
+.CurrentlyAlive:
+    cmp     al, 2
+    je      .RemainLive
+    cmp     al, 3
+    je      .RemainLive
+
+    jmp     .BecomeDead
+
+
+.CurrentlyDead:
+    cmp     al, 3
+    je      .BecomeLive
+
+    jmp     .RemainDead
+
+
+.RemainLive:
+.BecomeLive:
+    mov     al, 1
+    jmp     .After
+
+
+.RemainDead:
+.BecomeDead:
+    mov     al, 0
+
+
+.After:
+
+
+    ; Set next state.
+    mov     [es:bp], al
+    inc     bp
+
+    ; Set pixel colour.
+    test    dl, dl
+    mov     al, 0
+    jz      .Dead
+    mov     al, 0xf
+.Dead:
+
+
+    ; Set pixel
+    push    ax
+    movzx   ax, ch
+    mov     dx, 320
+    mul     dx
+    movzx   dx, cl
+    add     ax, dx
+    mov     bx, ax
+    pop     ax
+
+    push    ds
+    mov     dx, 0xa000
+    mov     ds, dx
+    mov     [bx], al
+    pop     ds
+
+
+    pop     cx
     inc     cl
 
 .XCond:
     cmp     cl, 0xff
-    jb      short .Body
+    jb      .Body
 
-    xor     cl, cl
+    mov     cl, cl
     inc     ch
 
 .YCond:
     cmp     ch, 0x7f
-    jb      short .Body
+    jb      .Body
+
     ret
 
 
