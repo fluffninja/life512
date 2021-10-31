57f22f4ac8cf0633576df157379d7741c3848e21
fluffninja <fluffninja@outlook.com>
2021-10-31 23:11:07 +0000

    Reduce segment swapping, define constants

--- boot.asm
+++ boot.asm
@@ -2,12 +2,28 @@
     org 0x7c00
 
     ; Key Memory Addresses:
-    ;   0'0000:0'7BFF - Stack.
-    ;   0'7C00:0'7DFF - This Code.
-    ;   1'0000:1'FFFF - World State A.
-    ;   2'0000:2'FFFF - World State B.
-    ;   A'0000:A'FFFF - VGA VRAM.
-    ;   F'0000:F'FFFF - BIOS ROM.
+    ;   0'0000..0'7BFF - Stack.
+    ;   0'7C00..0'7DFF - This Code.
+    ;   1'0000..1'FFFF - World State A.
+    ;   2'0000..2'FFFF - World State B.
+    ;   A'0000..A'FFFF - VGA VRAM.
+    ;   F'0000..F'FFFF - BIOS ROM.
+
+    ; Non-configurable:
+
+%define WORLD_WIDTH             256
+%define WORLD_HEIGHT            256
+%define VIDEO_WIDTH             320
+%define VIDEO_HEIGHT            200
+%define VRAM_SEGMENT            0xA000
+%define BIOS_SEGMENT            0xF000
+
+    ; Configurable:
+
+%define STATE_A_SEGMENT         0x1000
+%define STATE_B_SEGMENT         0x2000
+%define DEAD_COLOUR             0x01
+%define LIVE_COLOUR             0x0f
 
 Start:
     cli
@@ -20,8 +36,6 @@ Start:
     mov     ss, ax
     mov     sp, 0x7c00
 
-    sti
-    
     ; Set graphics mode to 320x200 VGA.
     mov     ax, 0x0013
     int     0x10
@@ -32,15 +46,15 @@ Start:
     ; To provide some interesting initial state values, copy the raw content of
     ; the BIOS ROM (F'0000) into World State A (1'0000).
 
-    mov     ax, 0xf000
+    mov     ax, BIOS_SEGMENT
     mov     ds, ax
     xor     si, si
 
-    mov     ax, 0x1000
+    mov     ax, STATE_A_SEGMENT
     mov     es, ax
     xor     di, di
 
-    mov     cx, (256 * 256) / 2     ; Copy in word-sized units.
+    mov     cx, (WORLD_WIDTH * WORLD_HEIGHT) / 2
 
 .InitLoop:
     lodsw
@@ -53,23 +67,21 @@ Start:
     ; =========
     ; cx = Segment containing world data for current step.
     ; dx = Segment containing world data for next step.
-    mov     cx, 0x1000
-    mov     dx, 0x2000
+    mov     cx, STATE_A_SEGMENT
+    mov     dx, STATE_B_SEGMENT
 
 .MainLoop:
     mov     ds, cx
     mov     es, dx
-
-    push    cx
-    push    dx
     call    TickAndRender
 
-    ; Pop in reverse order to swap the world data.
-    pop     cx
-    pop     dx
+    mov     cx, es
+    mov     dx, ds
 
     ; Delay by waiting for the timer interrupt.
+    sti
     hlt
+    cli
 
     jmp     .MainLoop
 
@@ -78,49 +90,47 @@ TickAndRender:
     ; Params:
     ;   DS - World data for current step.
     ;   ES - World data for next step.
+    ;   SS - VGA VRAM.
     ; Locals:
     ;   CL - World X coord.
     ;   CH - World Y coord.
     ;   DI - Cell index in next step world data.
-    ;   BX - Memory access.
+    ;   BX, BP - Memory access.
     ;   AX, DX, SI - Scratch.
+
+    ; This function doesn't use the stack, so use SS as a third data segment.
+    mov     ax, VRAM_SEGMENT
+    mov     ss, ax
+
     xor     cx, cx
     xor     di, di
 
-.Body:
+.LoopCells:
     ; AL - Live neighbour count.
     ; AH - Current cell state.
     xor     ax, ax
 
     ; BL and BH are current cell X and Y.
-    ; Use INC and DEC to get neighbours.
     ; Rely on integer overflow to make world wrap at borders.
     mov     bx, cx
 
     ; Row above.
     dec     bh
-    dec     bl
+    add     al, [bx - 1]
     add     al, [bx]
-    inc     bl
-    add     al, [bx]
-    inc     bl
-    add     al, [bx]
+    add     al, [bx + 1]
 
     ; Current row.
     inc     bh
-    add     al, [bx]
-    dec     bl
-    mov     ah, [bx]
-    dec     bl
-    add     al, [bx]
+    add     al, [bx - 1]
+    mov     ah, [bx]                ; Current cell.
+    add     al, [bx + 1]
 
     ; Row below.
     inc     bh
+    add     al, [bx - 1]
     add     al, [bx]
-    inc     bl
-    add     al, [bx]
-    inc     bl
-    add     al, [bx]
+    add     al, [bx + 1]
 
     ; Evaluate next state.
     test    ah, ah
@@ -148,18 +158,18 @@ TickAndRender:
     stosb
 
     ; Choose pixel colour.
-    mov     al, 0x01                ; Dead.
+    mov     al, DEAD_COLOUR
     test    ah, ah
     jz      .Dead
-    mov     al, 0x0f                ; Live.
+    mov     al, LIVE_COLOUR
 .Dead:
 
     ; Set pixel...
 
     ; Skip if Y is off-screen.
-    cmp     ch, (256 - 200) / 2
+    cmp     ch, (WORLD_HEIGHT - VIDEO_HEIGHT) / 2
     jb      .NoDraw
-    cmp     ch, 256 - (256 - 200) / 2
+    cmp     ch, WORLD_HEIGHT - (WORLD_HEIGHT - VIDEO_HEIGHT) / 2
     jae     .NoDraw
 
     ; Save AX.
@@ -167,29 +177,25 @@ TickAndRender:
 
     ; Calculate pixel offset.
     movzx   ax, ch
-    add     ax, (200 - 256) / 2
-    mov     dx, 320
+    add     ax, (VIDEO_HEIGHT - WORLD_HEIGHT) / 2
+    mov     dx, VIDEO_WIDTH
     mul     dx
     movzx   dx, cl
     add     ax, dx
-    add     ax, (320 - 256) / 2
+    add     ax, (VIDEO_WIDTH - WORLD_WIDTH) / 2
     mov     bp, ax
 
-    ; Save DS.
-    mov     si, ds
-
     ; Put pixel.
-    mov     dx, 0xa000
-    mov     ds, dx
-    mov     [ds:bp], bl
-
-    ; Recall DS.
-    mov     ds, si
+    mov     [ss:bp], bl
 .NoDraw:
 
     inc     cx
     test    cx, cx
-    jnz     .Body
+    jnz     .LoopCells
+
+    ; Restore stack segment.
+    xor     ax, ax
+    mov     ss, ax
 
     ret
 
